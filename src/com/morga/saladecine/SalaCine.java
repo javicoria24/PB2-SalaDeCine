11c1a190bba5b6316ff5ca20c2f315fa2df165b0
javier <javi_a15@live.com.ar>
2025-09-10 12:28:11 -0300

    se modifica clase main y sala de cine

--- src/com/morga/saladecine/SalaCine.java
+++ src/com/morga/saladecine/SalaCine.java
@@ -1,5 +1,42 @@
 package com.morga.saladecine;
 
 public class SalaCine {
+	private Asiento[][] butacas;
+	private Pelicula pelicula;
+
+	public SalaCine(int fila, int columna) {
+
+		this.butacas = new Asiento[fila][columna];
+
+		for (int i = 0; i < fila; i++) {
+
+			for (int j = 0; j < columna; j++) {
+				this.butacas[i][j] = new Asiento();
+			}
+
+		}
+
+	}
+
+	public Asiento[][] getButacas() {
+		return this.butacas;
+	}
+
+	public int contarAsientosOcupados() {
+		int contador = 0;
+
+		for (int i = 0; i < butacas.length; i++) {
+			for (int j = 0; j < butacas[i].length; j++) {
+				if (this.butacas[i][j].estaOcupado())
+					contador++;
+			}
+
+		}
+		return contador;
+	}
+
+	public int getTotalAsintos() {
+		return butacas.length * butacas[0].length;
+	}
 
 }
